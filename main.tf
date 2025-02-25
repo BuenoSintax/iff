e7b7edd3db185f699f71952a916d13c870a7eacd
BuenoSintax <extremeinsideps@gmail.com>
2025-02-25 19:41:37 -0300

    secstep

--- main.tf
+++ main.tf
@@ -1,12 +1,5 @@
-
-# Configuração do provedor AWS com credenciais embutidas
-provider "aws" {
-  region     = os.getenv("AWS_DEFAULT_REGION", "us-east-1")         # ajuste a região conforme necessário
-  access_key = os.getenv("AWS_ACCESS_KEY_ID")
-  secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
-}
-
-# Recurso: Criação de um bucket S3 básico
-resource "aws_s3_bucket" "meu_bucket" {
-  bucket = "nome-unico-do-bucket-12345"  # O nome deve ser único globalmente
+# Resource: Creating a basic S3 bucket
+resource "aws_s3_bucket" "mmybucket" {
+  # Lembre-se: bucket_name precisa ser único globalmente
+  bucket = "ifftest1"  
 }
